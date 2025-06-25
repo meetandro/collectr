43a36a94f62bdd9105905cab54237f0fd9c9b299
Andro Bakashvili <bakashviliandro@gmail.com>
2025-06-25 18:07:50 +0400

    chore: add validation for merging collections

---
+++ CollectR.Application/Features/Collections/Commands/MergeCollection/MergeCollectionCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace CollectR.Application.Features.Collections.Commands.MergeCollection;
+
+public sealed class MergeCollectionCommandValidator : AbstractValidator<MergeCollectionCommand>
+{
+    public MergeCollectionCommandValidator()
+    {
+        RuleFor(x => x.File.FileName).NotEmpty().WithMessage("File name is required.");
+    }
+}
+
